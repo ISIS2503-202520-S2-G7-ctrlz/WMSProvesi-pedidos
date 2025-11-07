81f05a579c3266a03331b4e3f49ee87b62388cfc
Hernan Ramirez R <h.ramirezr@uniandes.edu.co>
2025-11-07 02:16:27 -0500

    fix database connection

--- WMSProvesi-pedidos/settings.py
+++ WMSProvesi-pedidos/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -82,7 +83,7 @@ DATABASES = {
         'NAME': 'provesi_db',
         'USER': 'provesi_user',
         'PASSWORD': 'isis2503',
-        'HOST': 'localhost',
+        'HOST': os.getenv('DATABASE_HOST', 'localhost'),
         'PORT': '5432',
     }
 }
