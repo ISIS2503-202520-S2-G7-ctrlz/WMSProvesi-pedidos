e1fd877ae8530e5df9b221b9688ac599981ca027
Hernan Ramirez R <h.ramirezr@uniandes.edu.co>
2025-11-07 01:59:10 -0500

    Update paths

--- WMSProvesi-pedidos/urls.py
+++ WMSProvesi-pedidos/urls.py
@@ -15,9 +15,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from . import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('health/', views.health_check, name='health'),
     path('', include('pedidos.urls')),
     path('', include('productos.urls')),
 ]
